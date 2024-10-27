6f4d3eec3430b30decff3363ba01b3e2e4c9f712
samueljr0505 <samuelgonzalez5756@gmail.com>
2024-10-26 20:05:24 -0500

    trustee info

--- sc4p-frontend/src/pages/Profile.tsx
+++ sc4p-frontend/src/pages/Profile.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import { useAuth } from "../AuthContext";
 import { ContactDisplay } from "../components/EmergencyContactDisplay";
 import { ContactForm } from "../components/EmergencyContactForm";
+import { TrusteeForm } from "../components/TrusteeForm"; 
+import { TrusteeDisplay } from "../components/TrusteeDisplay";
 import { Button } from "@nextui-org/react";
 
 type User = {
@@ -21,6 +23,21 @@ interface Contact {
   email: string;
 }
 
+interface Trustee {
+  id: string;
+  fullName: string;
+  address: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  homePhone: string;
+  cellPhone: string;
+  email: string;
+  yearlyAllocation: string;
+  fundSource: string;
+  customSource?: string;
+}
+
 const Profile: React.FC = () => {
   const { logout, getUser } = useAuth();
   const navigate = useNavigate();
@@ -30,6 +47,8 @@ const Profile: React.FC = () => {
   const currentUser = getUser();
   const [contacts, setContacts] = useState<Contact[]>([]);
   const [showForm, setShowForm] = useState(false);
+  const [trustees, setTrustees] = useState<Trustee[]>([]); 
+  const [showTrusteeForm, setShowTrusteeForm] = useState(false);
 
   const addContact = (newContact: Contact) => {
     setContacts((prevContacts) => [
@@ -37,6 +56,14 @@ const Profile: React.FC = () => {
       { ...newContact, id: Date.now().toString() },
     ]);
     setShowForm(false);
+  };
+
+  const addTrustee = (newTrustee: Trustee) => {
+    setTrustees((prevTrustees) => [
+      ...prevTrustees,
+      { ...newTrustee, id: Date.now().toString() },
+    ]);
+    setShowTrusteeForm(false);
   };
 
   useEffect(() => {
@@ -91,6 +118,29 @@ const Profile: React.FC = () => {
           />
         )}
       </div>
+
+      <div>
+        <h1>Trustees</h1>
+        {trustees.length === 0 ? (
+          <p className="text-lg mb-4">No Trustees Added</p>
+        ) : (
+          trustees.map((trustee) => (
+            <TrusteeDisplay key={trustee.id} trustee={trustee} /> 
+          ))
+        )}
+        {!showTrusteeForm && (
+          <Button onClick={() => setShowTrusteeForm(true)}>
+            Add Trustee
+          </Button>
+        )}
+        {showTrusteeForm && (
+          <TrusteeForm
+            onSubmit={addTrustee}
+            onCancel={() => setShowTrusteeForm(false)}
+          />
+        )}
+      </div>
+
     </div>
   );
 };
