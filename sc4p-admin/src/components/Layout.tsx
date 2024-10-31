638b3cd039073b782ab76c0e648b5f87d1d42f07
JacksonLaVallee <jacksonclavallee@gmail.com>
2024-10-30 19:47:07 -0500

    Admin page implemented

---
+++ sc4p-admin/src/components/Layout.tsx
@@ -0,0 +1,19 @@
+// import React from "react";
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+const Layout = () => {
+  return (
+    <Box display="flex" width="100%" height="100%">
+      <Sidebar />
+      <Box flexGrow={1}>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
