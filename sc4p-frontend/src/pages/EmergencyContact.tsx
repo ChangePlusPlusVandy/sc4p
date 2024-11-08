0b182250298e52c54e3e29f5924b4a2b1dbf8a7d
JacksonLaVallee <jacksonclavallee@gmail.com>
2024-11-08 11:50:56 -0600

    EC page skeleton implemented

--- sc4p-frontend/src/pages/EmergencyContact.tsx
+++ sc4p-frontend/src/pages/EmergencyContact.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import { ContactDisplay } from "../components/EmergencyContactDisplay";
 import { ContactForm } from "../components/EmergencyContactForm";
-import { Button } from "@nextui-org/react";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+} from "@nextui-org/react";
 
 interface Contact {
   id: string;
@@ -47,6 +54,238 @@ const EmergencyContacts: React.FC = () => {
           onCancel={() => setShowContactForm(false)}
         />
       )}
+      <hr />
+      <h2 className="text-xl font-semibold">
+        Primary Trustee or Trustee Service
+      </h2>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          First and Last Name
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Cell Phone
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Email
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Address
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          City
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          State
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Zip
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Money allocated per year to Trustee to provide for caregiver
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <hr/>
+
+      <h1 className="text-3xl font-bold">Trust Fund</h1>
+      <p>
+        We encourage you to work with an attorney or financial planner to appropriately fund your trust so that the Trustee is able to access the funds
+      </p>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Please indicate how you plan to provide funds for the care of your pet <br />
+          <Dropdown>
+            <DropdownTrigger>
+              <Button variant="bordered">Select</Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Static Actions">
+              <DropdownItem key="new">Temp1</DropdownItem>
+              <DropdownItem key="copy">Temp2</DropdownItem>
+              <DropdownItem key="edit">Temp3</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </p>
+      </div>
+
+      <hr/>
+
+      <h1 className="text-3xl font-bold">Remaining Funds</h1>
+      <p>
+        Should my pet(s) die while under the care of a caregiver, I would like my remaining funds distributed to (percentages should total 100%)
+      </p>
+
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          2nd Chance 4 Pets
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+          1484 Pollard Road, No. 444, Los Gatos, CA 95032
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Other pet welfare organization
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Pet welfare Organization Address
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          City
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div><div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          State
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+      <div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Zip
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div><div className="space-y-4">
+        <p className="font-semibold">
+          {" "}
+          Other beneficiary
+          <Input
+            variant="bordered"
+            radius="lg"
+            color="secondary"
+            className="max-w-sm"
+          />
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <Button>Save</Button>
+      </div>
+
     </div>
   );
 };
