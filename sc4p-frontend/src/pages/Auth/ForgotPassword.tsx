af835fafa04dfd7c1874b30c2b0b804dc58ac453
Ashley <ashleylai28@gmail.com>
2024-11-15 16:28:12 -0600

    added styling for forgot password and register page (excluding input boxes)

--- sc4p-frontend/src/pages/Auth/ForgotPassword.tsx
+++ sc4p-frontend/src/pages/Auth/ForgotPassword.tsx
@@ -6,6 +6,7 @@ import * as Yup from "yup";
 import { useAuth } from "../../AuthContext";
 import FormError from "./FormError";
 import { Button, Input } from "@nextui-org/react";
+import Logo from "../../images/logo.png";
 
 interface FormValues {
   email: string;
@@ -50,25 +51,113 @@ const ForgotPassword: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Forgot Password</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <Input type="email" id="email" {...register("email")} />
-          {errors.email != null && (
-            <FormError>{errors.email.message}</FormError>
-          )}
+    <div className="flex h-screen">
+      <div className="flex-grow">
+        <div className="flex">
+          <img
+            src={Logo}
+            style={{
+              width: "55.6px",
+              height: "109.7px",
+              marginTop: "25.98px",
+              marginLeft: "33.9px",
+            }}
+          ></img>
+          <h1
+            style={{
+              width: "160.64px",
+              height: "78px",
+              marginTop: "48.4px",
+              marginLeft: "15px",
+              fontFamily: "Inter",
+              fontSize: "24px",
+              fontWeight: "700",
+              lineHeight: "28.73px",
+              textAlign: "left",
+              color: "#5E3593",
+            }}
+          >
+            2nd Chance For Pets
+          </h1>
         </div>
-        {error && <FormError>{error}</FormError>}
-        <Button disabled={isSubmitting} type="submit">
-          {isSubmitting ? "Submitting" : "Reset"}
-        </Button>
-        {message && <p>{message}</p>}
-      </form>
-      <p>
-        <Link to="/login">Back to login</Link>
-      </p>
+        <h1
+          style={{
+            marginLeft: "200px",
+            width: "350px",
+            height: "54px",
+            marginTop: "50px",
+            fontFamily: "Inter",
+            fontSize: "32px",
+            fontWeight: "800",
+            lineHeight: "40.41px",
+          }}
+        >
+          Forgot Password
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)} style={{ marginLeft: "200px" }}>
+          <div>
+            <label
+              htmlFor="email"
+              style={{
+                width: "380px",
+                display: "inline-block",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "19.05px",
+                marginTop: "10px",
+                marginBottom: "24px",
+              }}
+            >
+              Enter your email address or phone number and we will send you the
+              reset instruction
+            </label>
+            <Input type="email" id="email" {...register("email")} />
+            {errors.email != null && (
+              <FormError>{errors.email.message}</FormError>
+            )}
+          </div>
+          {error && <FormError>{error}</FormError>}
+          <Button
+            disabled={isSubmitting}
+            type="submit"
+            style={{
+              background: "#A377DC",
+              color: "white",
+              width: "160px",
+              height: "42px",
+              padding: "16px 24px 16px 24px",
+              gap: "14px",
+              borderRadius: "15px",
+              marginTop: "15px",
+              fontFamily: "Inter",
+              fontWeight: "600",
+            }}
+          >
+            {isSubmitting ? "Submitting" : "Reset Password"}
+          </Button>
+          {message && <p>{message}</p>}
+        </form>
+        <p
+          style={{
+            marginLeft: "200px",
+            fontFamily: "Inter",
+            fontWeight: "600",
+            color: "#5F17BE",
+            fontSize: "14px",
+            marginTop: "6px",
+          }}
+        >
+          <Link to="/login">Back to login</Link>
+        </p>
+      </div>
+      <div
+        style={{
+          width: "530.12px", // Exact width
+          backgroundColor: "#E6D1FF",
+        }}
+      ></div>
     </div>
   );
 };
--- sc4p-frontend/src/pages/Auth/Register.tsx
+++ sc4p-frontend/src/pages/Auth/Register.tsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { useAuth } from "../../AuthContext";
 import FormError from "./FormError";
+import Logo from "../../images/logo.png";
 
 import { Button, Input } from "@nextui-org/react";
 
@@ -13,6 +14,7 @@ interface FormValues {
   email: string;
   password: string;
   confirmPassword: string;
+  confirmEmail: string;
   address: string;
   city: string;
   state: string;
@@ -33,6 +35,9 @@ const schema = Yup.object().shape({
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords do not match")
     .required("Confirm password is required"),
+  confirmEmail: Yup.string()
+    .oneOf([Yup.ref("email")], "Emails do not match")
+    .required("Confirm email is required"),
   address: Yup.string().required("Address is required"),
   city: Yup.string().required("City is required"),
   state: Yup.string().required("State is required"),
@@ -84,63 +89,270 @@ const Register: React.FC = () => {
 
   return (
     <div>
-      <h1>Register</h1>
+      <div className="flex">
+        <img
+          src={Logo}
+          style={{
+            width: "55.6px",
+            height: "109.7px",
+            marginTop: "25.98px",
+            marginLeft: "33.9px",
+          }}
+        ></img>
+        <h1
+          style={{
+            width: "160.64px",
+            height: "78px",
+            marginTop: "48.4px",
+            marginLeft: "15px",
+            fontFamily: "Inter",
+            fontSize: "24px",
+            fontWeight: "700",
+            lineHeight: "28.73px",
+            textAlign: "left",
+            color: "#5E3593",
+          }}
+        >
+          2nd Chance For Pets
+        </h1>
+      </div>
+      <h1
+        style={{
+          marginLeft: "200px",
+          width: "110px",
+          height: "54px",
+          marginTop: "10px",
+          fontFamily: "Inter",
+          fontSize: "32px",
+          fontWeight: "800",
+          lineHeight: "48.41px",
+        }}
+      >
+        Register
+      </h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <Input type="text" id="name" {...register("name")} />
-          {errors.name != null && <FormError>{errors.name.message}</FormError>}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(2, 350px)",
+            gridTemplateRows: "repeat(3, auto)",
+            columnGap: "40px",
+            rowGap: "8px", // Space between columns
+
+            marginLeft: "200px",
+          }}
+        >
+          <div>
+            <label
+              htmlFor="name"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              Name
+            </label>
+            <Input type="text" id="name" {...register("name")} />
+            {errors.name != null && (
+              <FormError>{errors.name.message}</FormError>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="workPhone"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              Work Phone
+            </label>
+            <Input type="text" id="workPhone" {...register("workPhone")} />
+            {errors.workPhone != null && (
+              <FormError>{errors.workPhone.message}</FormError>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              Password
+            </label>
+            <Input type="password" id="password" {...register("password")} />
+            {errors.password != null && (
+              <FormError>{errors.password.message}</FormError>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="confirmPassword"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              Confirm Password
+            </label>
+            <Input
+              type="password"
+              id="confirmPassword"
+              {...register("confirmPassword")}
+            />
+            {errors.confirmPassword && (
+              <FormError>{errors.confirmPassword.message}</FormError>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              Email
+            </label>
+            <Input type="email" id="email" {...register("email")} />
+            {errors.email != null && (
+              <FormError>{errors.email.message}</FormError>
+            )}
+          </div>
+
+          <div>
+            <label
+              htmlFor="confirmEmail"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              Confirm Email
+            </label>
+            <Input
+              type="email"
+              id="confirmEmail"
+              {...register("confirmEmail")}
+            />
+            {errors.confirmEmail && (
+              <FormError>{errors.confirmEmail.message}</FormError>
+            )}
+          </div>
         </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <Input type="email" id="email" {...register("email")} />
-          {errors.email != null && (
-            <FormError>{errors.email.message}</FormError>
-          )}
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <Input type="password" id="password" {...register("password")} />
-          {errors.password != null && (
-            <FormError>{errors.password.message}</FormError>
-          )}
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <Input
-            type="password"
-            id="confirmPassword"
-            {...register("confirmPassword")}
-          />
-          {errors.confirmPassword && (
-            <FormError>{errors.confirmPassword.message}</FormError>
-          )}
-        </div>
-        <div>
-          <label htmlFor="address">Address</label>
+        <div
+          style={{
+            marginLeft: "200px",
+          }}
+        >
+          <label
+            htmlFor="address"
+            style={{
+              width: "121px",
+              height: "29px",
+              fontFamily: "Inter",
+              fontSize: "17px",
+              fontWeight: "700",
+              lineHeight: "39.05px",
+            }}
+          >
+            Address
+          </label>
           <Input type="text" id="address" {...register("address")} />
           {errors.address != null && (
             <FormError>{errors.address.message}</FormError>
           )}
         </div>
-        <div>
-          <label htmlFor="city">City</label>
-          <Input type="text" id="city" {...register("city")} />
-          {errors.city != null && <FormError>{errors.city.message}</FormError>}
+        <div
+          className="flex"
+          style={{
+            marginLeft: "200px",
+            gap: "40px",
+          }}
+        >
+          <div>
+            <label
+              htmlFor="city"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              City
+            </label>
+            <Input type="text" id="city" {...register("city")} />
+            {errors.city != null && (
+              <FormError>{errors.city.message}</FormError>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="state"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              State
+            </label>
+            <Input type="text" id="state" {...register("state")} />
+            {errors.state != null && (
+              <FormError>{errors.state.message}</FormError>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="zip"
+              style={{
+                width: "121px",
+                height: "29px",
+                fontFamily: "Inter",
+                fontSize: "17px",
+                fontWeight: "700",
+                lineHeight: "39.05px",
+              }}
+            >
+              Zip
+            </label>
+            <Input type="text" id="zip" {...register("zip")} />
+            {errors.zip != null && <FormError>{errors.zip.message}</FormError>}
+          </div>
         </div>
-        <div>
-          <label htmlFor="state">State</label>
-          <Input type="text" id="state" {...register("state")} />
-          {errors.state != null && (
-            <FormError>{errors.state.message}</FormError>
-          )}
-        </div>
-        <div>
-          <label htmlFor="zip">Zip</label>
-          <Input type="text" id="zip" {...register("zip")} />
-          {errors.zip != null && <FormError>{errors.zip.message}</FormError>}
-        </div>
-        <div>
+        {/* <div>
           <label htmlFor="homePhone">Home Phone</label>
           <Input type="text" id="homePhone" {...register("homePhone")} />
           {errors.homePhone != null && (
@@ -153,21 +365,42 @@ const Register: React.FC = () => {
           {errors.cellPhone != null && (
             <FormError>{errors.cellPhone.message}</FormError>
           )}
-        </div>
-        <div>
-          <label htmlFor="workPhone">Work Phone</label>
-          <Input type="text" id="workPhone" {...register("workPhone")} />
-          {errors.workPhone != null && (
-            <FormError>{errors.workPhone.message}</FormError>
-          )}
-        </div>
+        </div> */}
+
         {error && <FormError>{error}</FormError>}
-        <Button disabled={isSubmitting} type="submit">
-          {isSubmitting ? "Submitting" : "Register"}
+        <Button
+          disabled={isSubmitting}
+          type="submit"
+          style={{
+            background: "#A377DC",
+            color: "white",
+            width: "100px",
+            height: "42px",
+            padding: "16px 24px 16px 24px",
+            gap: "14px",
+            borderRadius: "15px",
+            marginTop: "15px",
+            fontFamily: "Inter",
+            fontWeight: "600",
+            marginLeft: "200px",
+          }}
+        >
+          {isSubmitting ? "Submitting" : "Sign Up"}
         </Button>
       </form>
-      <p>
-        Already have an account? <Link to="/login">Login</Link>
+      <p
+        style={{
+          marginLeft: "200px",
+          fontFamily: "Inter",
+          fontSize: "14px",
+          fontWeight: "600",
+          lineHeight: "41.78px",
+        }}
+      >
+        Already have an account?{" "}
+        <span style={{ color: "#5F17BE", textDecoration: "underline" }}>
+          <Link to="/login">Login</Link>
+        </span>
       </p>
     </div>
   );
