011ea25205872a7fc785d2c85b2d9a2a677981d8
josephjquinn <jquinn2005@outlook.com>
2024-11-15 19:50:30 -0600

    small sidebar ui adjustments

--- sc4p-frontend/src/components/Sidebar.tsx
+++ sc4p-frontend/src/components/Sidebar.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import { Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
 import HomeIcon from "../images/dashboard.png";
 import PetsIcon from "../images/pet-profile.png";
 import FormsIcon from "../images/Forms.png";
 import MoreIcon from "../images/more.png";
 import ProfileIcon from "../images/owner-profile.png";
-
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 const CustomSidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
 
@@ -16,69 +15,98 @@ const CustomSidebar = () => {
   };
 
   return (
-    <div className="flex h-screen"> {/* Full height container */}
-      <Sidebar
-        collapsed={collapsed}
-        style={{
-          backgroundColor: "rgba(230, 209, 255, 1)",
-        }}
-      >
-        <div className="flex items-center p-2">
-          <button onClick={handleToggleSidebar} className="text-lg">
-            <FaBars />
-          </button>
-        </div>
+    <div className="flex h-screen">
+      <Sidebar collapsed={collapsed} className="bg-[#E6D1FF]">
+        <style>
+          {`
+            .ps-submenu-expand-icon.css-um1o6k {
+              display: none; /* Hide the submenu expand icon */
+            }
+          `}
+        </style>
+
         <Menu>
-          <MenuItem 
-            component={<Link to="/" />} 
-            icon={<img src={HomeIcon} alt="Dashboard" className="mr-2 w-5 h-5" />}
+          {collapsed ? (
+            <MenuItem
+              icon={<FaAngleDoubleRight />}
+              onClick={handleToggleSidebar}
+            ></MenuItem>
+          ) : (
+            <MenuItem
+              suffix={<FaAngleDoubleLeft />}
+              onClick={handleToggleSidebar}
+            >
+              <div
+                style={{
+                  textAlign: "center",
+                  padding: "9px",
+                  textTransform: "uppercase",
+                  fontWeight: "bold",
+                  fontSize: 15,
+                  letterSpacing: "1px",
+                }}
+              >
+                Directory
+              </div>
+            </MenuItem>
+          )}
+        </Menu>
+        <Menu>
+          <MenuItem
+            component={<Link to="/" />}
+            icon={
+              <img src={HomeIcon} alt="Dashboard" className="mr-2 w-5 h-5" />
+            }
           >
             <span className="font-bold text-black">Dashboard</span>
           </MenuItem>
-          <SubMenu 
-            label={<span className="font-bold text-black">Forms</span>} 
+          <SubMenu
+            label={<span className="font-bold text-black bg-red">Forms</span>}
             icon={<img src={FormsIcon} alt="Forms" className="mr-2 w-5 h-5" />}
           >
-            <MenuItem 
-              component={<Link to="/pets" />} 
-              style={{ backgroundColor: "rgba(230, 209, 255, .25)" }}
-            >
+            <MenuItem component={<Link to="/pets" />} className="bg-[#E6D1FF]">
               <span className="font-bold text-black">Pets</span>
             </MenuItem>
-            <MenuItem 
-              component={<Link to="/caregivers" />} 
-              style={{ backgroundColor: "rgba(230, 209, 255, .25)" }}
+            <MenuItem
+              component={<Link to="/caregivers" />}
+              className="bg-[#E6D1FF]"
             >
               <span className="font-bold text-black">Caregivers</span>
             </MenuItem>
-            <MenuItem 
-              component={<Link to="/trustees" />} 
-              style={{ backgroundColor: "rgba(230, 209, 255, .25)" }}
+            <MenuItem
+              component={<Link to="/trustees" />}
+              className="bg-[#E6D1FF]"
             >
               <span className="font-bold text-black">Trust</span>
             </MenuItem>
-            <MenuItem 
-              component={<Link to="/contacts" />} 
-              style={{ backgroundColor: "rgba(230, 209, 255, .25)" }}
+            <MenuItem
+              component={<Link to="/contacts" />}
+              className="bg-[#E6D1FF]"
             >
               <span className="font-bold text-black">Emergency Contacts</span>
             </MenuItem>
           </SubMenu>
-          <MenuItem 
-            component={<Link to="/profile" />} 
-            icon={<img src={ProfileIcon} alt="Profile" className="mr-2 w-5 h-5" />}
+          <MenuItem
+            component={<Link to="/profile" />}
+            icon={
+              <img src={ProfileIcon} alt="Profile" className="mr-2 w-5 h-5" />
+            }
           >
             <span className="font-bold text-black">Owner Profile</span>
           </MenuItem>
-          <MenuItem 
-            component={<Link to="/pets" />} 
-            icon={<img src={PetsIcon} alt="Pet Profile" className="mr-2 w-5 h-5" />}
+          <MenuItem
+            component={<Link to="/pets" />}
+            icon={
+              <img src={PetsIcon} alt="Pet Profile" className="mr-2 w-5 h-5" />
+            }
           >
             <span className="font-bold text-black">Pet Profile</span>
           </MenuItem>
-          <MenuItem 
-            component={<Link to="/more" />} 
-            icon={<img src={MoreIcon} alt="More Options" className="mr-2 w-5 h-5" />}
+          <MenuItem
+            component={<Link to="/more" />}
+            icon={
+              <img src={MoreIcon} alt="More Options" className="mr-2 w-5 h-5" />
+            }
           >
             <span className="font-bold text-black">More</span>
           </MenuItem>
