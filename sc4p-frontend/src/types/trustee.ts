c4feea002345f05f1f38bac177e6607e1f0c9e6b
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2025-01-13 22:34:46 -0600

    adding types to frontend

---
+++ sc4p-frontend/src/types/trustee.ts
@@ -0,0 +1,33 @@
+import { Decimal } from "@prisma/client/runtime/library";
+
+export interface Trustee {
+  id: number;
+  trust_fund_id: number;
+  trustee_name: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  home_phone: string | null;
+  cell_phone: string;
+  emergency_phone: string | null;
+  email: string;
+  allocated_amount: Decimal | null;
+  created_at: Date;
+}
+
+export interface CreateTrustee {
+  trust_fund_id: number;
+  trustee_name: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  home_phone?: string;
+  cell_phone: string;
+  emergency_phone?: string;
+  email: string;
+  allocated_amount?: number;
+}
+
+export interface UpdateTrustee extends Partial<CreateTrustee> {}
